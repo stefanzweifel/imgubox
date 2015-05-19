049b2c9d328571585f66dae71ae893f12a59cf5d
Stefan Zweifel <hello@stefanzweifel.io>
2015-05-19 20:26:47 +0200

    Scheduled commands shouldn't overlap

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->command('imgubox:fetchFavs')->everyTenMinutes();
+		$schedule->command('imgubox:fetchFavs')->everyTenMinutes()->withoutOverlapping();;
 	}
 
 }
