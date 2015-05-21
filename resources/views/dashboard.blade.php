e9a066c9ee099200e9f0f812c9572c073405b308
Stefan Zweifel <hello@stefanzweifel.io>
2015-05-21 10:16:53 +0200

    Fix an Issue on Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
         <p><b>ImguBox</b> is still in it's early days. If you find  bugs or think something should be improved, tweet us <a href="https://twitter.com/@imguboxapp">@imguboxapp</a>.</p>
     </div>
 
-    @if (Auth::user()->hasImgurToken()->first() && Auth::user()->hasDropboxToken()->first())
+    @if (Auth::user()->imgurToken && Auth::user()->dropboxToken)
 
         @if (Auth::user()->logs->count() <= 0)
 
@@ -32,7 +32,7 @@
     <div class="row">
         <div class="col-md-6">
 
-           @if (Auth::user()->hasImgurToken()->first() )
+           @if (Auth::user()->imgurToken )
                 <div class="panel panel-success">
             @else
                <div class="panel panel-default">
@@ -47,7 +47,7 @@
 
                     <p>Connect with Imgur so we can grab your favorites for you.</p>
 
-                    @if (Auth::user()->hasImgurToken()->first() )
+                    @if (Auth::user()->imgurToken )
                         <a href="#" class="disabled btn btn-success">Already connected</a>
                     @else
                         {!! link_to_route('auth.imgur.redirect', 'Connect', [], ['class' => 'btn btn-success']) !!}
@@ -60,7 +60,7 @@
 
         <div class="col-md-6">
 
-           @if (Auth::user()->hasDropboxToken()->first() )
+           @if (Auth::user()->dropboxToken )
                 <div class="panel panel-success">
             @else
                <div class="panel panel-default">
@@ -74,7 +74,7 @@
 
                     <p>Choose Dropbox-Account where we should store your favorites.</p>
 
-                    @if (Auth::user()->hasDropboxToken()->first() )
+                    @if (Auth::user()->dropboxToken )
                         <a href="#" class="disabled btn btn-success">Already connected</a>
                     @else
                         {!! link_to_route('auth.dropbox.redirect', 'Connect', [], ['class' => 'btn btn-success']) !!}
