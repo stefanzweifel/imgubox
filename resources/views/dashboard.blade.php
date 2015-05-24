17e57f731da9640280d282d579c18a72f270aab4
Stefan Zweifel <hello@stefanzweifel.io>
2015-05-24 10:01:20 +0200

    Squashed commit of the following:
    commit 4045b8c689d35170675b5d26c4809b9c9bf9b2a2
    Author: Stefan Zweifel <hello@stefanzweifel.io>
    Date:   Sun May 24 10:01:07 2015 +0200
    
        Wording changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,10 @@
         <p><b>ImguBox</b> is still in it's early days. If you find  bugs or think something should be improved, tweet us <a href="https://twitter.com/@imguboxapp">@imguboxapp</a>.</p>
     </div>
 
+    <div class="page-header">
+        <h2>Setup</h2>
+    </div>
+
     @if (Auth::user()->imgurToken && Auth::user()->dropboxToken)
 
         @if (Auth::user()->logs->count() <= 0)
@@ -18,16 +22,13 @@
 
             <div class="alert alert-info" role="alert">
                 <p>Last successfull sync: <b>{!! Auth::user()->logs()->latest()->first()->created_at->format('d.m.Y H:i:s') !!} ({!! Auth::user()->logs()->latest()->first()->created_at->timezoneName !!})</b>.</p>
-                <p>We synced <b>{{ Auth::user()->logs->count() }}</b> images for you so far.</p>
             </div>
 
         @endif
 
     @endif
 
-    <div class="page-header">
-        <h2>Setup</h2>
-    </div>
+    <p>You can manage your connections in your {!! link_to_route('user.settings', 'Settings page') !!}.</p>
 
     <div class="row">
         <div class="col-md-6">
@@ -45,12 +46,12 @@
 
                 <div class="panel-body">
 
-                    <p>Connect with Imgur so we can grab your favorites for you.</p>
+                    <p>Connect ImguBox with Imgur so we can see which images and albums you have favorited.</p>
 
                     @if (Auth::user()->imgurToken )
-                        <a href="#" class="disabled btn btn-success">Already connected</a>
+                        <a href="#" class="disabled btn btn-success">Authorized</a>
                     @else
-                        {!! link_to_route('auth.imgur.redirect', 'Connect', [], ['class' => 'btn btn-success']) !!}
+                        {!! link_to_route('auth.imgur.redirect', 'Authorize', [], ['class' => 'btn btn-success']) !!}
                     @endif
 
                 </div>
@@ -72,12 +73,12 @@
 
                 <div class="panel-body">
 
-                    <p>Choose Dropbox-Account where we should store your favorites.</p>
+                    <p>Connect ImguBox with Dropbox so we know in which Dropbox we should put your images. (Images will appear in your <code>App/ImguBox</code> folder)</p>
 
                     @if (Auth::user()->dropboxToken )
-                        <a href="#" class="disabled btn btn-success">Already connected</a>
+                        <a href="#" class="disabled btn btn-success">Authorized</a>
                     @else
-                        {!! link_to_route('auth.dropbox.redirect', 'Connect', [], ['class' => 'btn btn-success']) !!}
+                        {!! link_to_route('auth.dropbox.redirect', 'Authorize', [], ['class' => 'btn btn-success']) !!}
                     @endif
                 </div>
             </div>
