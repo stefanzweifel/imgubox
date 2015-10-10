cec6338c7faebf0f4911c943dab0faf396736158
Stefan Zweifel <hello@stefanzweifel.io>
2015-10-10 21:40:40 +0200

    New ModelFactory

--- tests/factories/factories.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$factory('ImguBox\User', [
-
-    'email'          => $faker->email,
-    'password'       => Hash::make('super-secure-password'),
-    'imgur_username' => $faker->username
-
-]);
