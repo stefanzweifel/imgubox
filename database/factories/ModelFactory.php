d522339d6a36e2a36b85c147b72931950658afc6
Stefan Zweifel <hello@stefanzweifel.io>
2015-10-10 22:24:35 +0200

    Add new Model Factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,27 @@ $factory->define(ImguBox\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(ImguBox\Log::class, function (Faker\Generator $faker) {
+    return [
+        'user_id'  => factory(ImguBox\User::class)->create()->id,
+        'imgur_id' =>str_random(10),
+        'is_album' => $faker->boolean()
+    ];
+});
+
+$factory->define(ImguBox\Provider::class, function (Faker\Generator $faker) {
+    return [
+        'name'       => $faker->company,
+        'short_name' => str_slug($faker->company),
+        'is_storage' => $faker->boolean
+    ];
+});
+$factory->define(ImguBox\Token::class, function (Faker\Generator $faker) {
+    return [
+        'token'         => str_random(10),
+        'refresh_token' => str_random(10),
+        'provider_id'   => factory(ImguBox\Provider::class)->create()->id,
+        'user_id'       => factory(ImguBox\User::class)->create()->id,
+    ];
+});
