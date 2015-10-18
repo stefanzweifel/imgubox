195ec69333e9d000dea7ef2d192972dd0c729ada
Stefan Zweifel <hello@stefanzweifel.io>
2015-10-18 21:06:27 +0200

    Changed wording in api-error email

--- resources/views/emails/api-error.blade.php
+++ resources/views/emails/api-error.blade.php
@@ -1,6 +1,10 @@
 <p>Hi there</p>
 
 <p>Looks like your Imgur Token on <a href="http://imgubox.wnx.ch">Imgubox</a> is no longer valid. We had to delete your token.</p>
-<p>If your Imgur favorites should appear in your Dropbox again, you have to reauthorize Imgur at <a href="http://imgubox.wnx.ch">Imgubox</a>.</p>
+<p>If your Imgur favorites should appear in your Dropbox again, you have to reauthorize Imgur.</p>
+
+<p>
+    <a href="http://imgubox.wnx.ch">Reauthorize Imgur now</a>
+</p>
 
 <p>Your ImguBox Team</p>
