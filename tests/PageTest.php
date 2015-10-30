dc661d5f7c1a87e65242ef84c2e049624eecf352
Stefan Zweifel <hello@stefanzweifel.io>
2015-10-30 10:18:49 +0100

    Add more tests for settings page
    - See delete buttons for tokens
    - Test you can delete tokens

--- tests/PageTest.php
+++ tests/PageTest.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use ImguBox\Provider;
+use ImguBox\Token;
+use ImguBox\User;
 
 class PageTest extends TestCase
 {
@@ -20,13 +23,84 @@ class PageTest extends TestCase
 
     public function testItLoadsDashboardView()
     {
-        $user = factory(ImguBox\User::class)->create();
+        $user = factory(User::class)->create();
         $this->actingAs($user)->visit('/')->see('You can manage your connections');
     }
 
     public function testItLoadsSettingsView()
     {
-        $user = factory(ImguBox\User::class)->create();
+        $user = factory(User::class)->create();
+
         $this->actingAs($user)->visit('/settings')->see('Settings');
     }
+
+    public function testItLoadsSettingsViewAndYouCanConnectToImgur()
+    {
+        $user = factory(User::class)->create();
+
+        $dropbox = factory(Token::class)->create([
+            'user_id' => $user->id,
+            'provider_id' => factory(Provider::class, 'Dropbox')->create()->id
+        ]);
+
+
+        $this->actingAs($user)->visit('/settings')->see('Connect');
+    }
+
+    public function testItLoadsSettingsViewAndYouCanConnectToDropbox()
+    {
+        $user = factory(User::class)->create();
+        $imgur = factory(Token::class)->create([
+            'user_id' => $user->id,
+            'provider_id' => factory(Provider::class, 'Imgur')->create()->id
+        ]);
+
+        $this->actingAs($user)->visit('/settings')->see('Connect');
+    }
+
+    public function testItLoadsSettingsViewAndYouCanDeleteTokens()
+    {
+        $user = factory(User::class)->create();
+        $imgur = factory(Token::class)->create([
+            'user_id' => $user->id,
+            'provider_id' => factory(Provider::class, 'Imgur')->create()->id
+        ]);
+
+        $dropbox = factory(Token::class)->create([
+            'user_id' => $user->id,
+            'provider_id' => factory(Provider::class, 'Dropbox')->create()->id
+        ]);
+
+        $this->actingAs($user)->visit('/settings')->see('Delete');
+    }
+
+    public function testYouCanDeleteDropboxToken()
+    {
+        $user = factory(User::class)->create();
+
+        $dropbox = factory(Token::class)->create([
+            'user_id' => $user->id,
+            'provider_id' => factory(Provider::class, 'Dropbox')->create()->id
+        ]);
+
+        $this->actingAs($user)->visit('/settings')->click('Delete');
+
+        $this->assertEquals(null, $user->dropboxToken->first());
+    }
+
+    public function testYouCanDeleteImgurToken()
+    {
+        $user = factory(User::class)->create();
+
+        $dropbox = factory(Token::class)->create([
+            'user_id' => $user->id,
+            'provider_id' => factory(Provider::class, 'Imgur')->create()->id
+        ]);
+
+        $this->actingAs($user)->visit('/settings')->click('Delete');
+
+        $this->assertEquals(null, $user->imgurToken->first());
+    }
+
+
 }
