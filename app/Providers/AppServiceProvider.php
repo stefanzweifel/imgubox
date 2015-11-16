c31a63cae415496caa65edcae4eedac5b88c4edc
Stefan Zweifel <hello@stefanzweifel.io>
2015-11-16 22:18:37 +0100

    Add Dropbox Client
    - Wrapper around GrahamCampbell\Dropbox\DropboxManager
    - Add Interface / Contract StorageProvider
    - Add Interface-Implementation Binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,8 @@
 <?php namespace ImguBox\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use ImguBox\Contracts\StorageProvider;
+use ImguBox\Services\Dropbox\Client as DropboxClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() == 'local') {
             $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
         }
+
+        $this->app->bind(StorageProvider::class, DropboxClient::class);
     }
 }
