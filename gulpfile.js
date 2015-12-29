358b1c9e5d91ba52aa0c8e02d79b6e16037de333
Stefan Zweifel <hello@stefanzweifel.io>
2015-12-29 15:52:47 +0100

    Use new marketing.scss in gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -20,13 +20,14 @@ var paths = {
 elixir(function(mix) {
 
     mix.sass("app.scss", 'public/css/', {includePaths: [paths.bootstrap + 'stylesheets/']})
+        .sass("marketing.scss", 'public/css/', {includePaths: [paths.bootstrap + 'stylesheets/']})
         .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
         .scripts([
             'vendor/jquery/dist/jquery.js',
             'vendor/bootstrap/dist/js/bootstrap.min.js',
             'js/app.js',
         ], 'public/js/app.js', 'resources/')
-        .version(['css/app.css', 'js/app.js']);
+        .version(['css/app.css', 'css/marketing.css', 'js/app.js']);
 
     mix.phpUnit();
 
