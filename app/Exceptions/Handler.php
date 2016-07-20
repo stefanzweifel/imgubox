7b5b721776e6bd37199280bfabb6a02a7c50f3eb
Stefan Zweifel <hello@stefanzweifel.io>
2016-07-20 21:11:01 +0200

    Remove jenssegers/raven

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -35,10 +35,6 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $e)
     {
-        if (!config('app.debug')) {
-            Log::error($e);
-        }
-
         return parent::report($e);
     }
 
